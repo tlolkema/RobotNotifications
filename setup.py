1e1237bf68aa68ccee92f19d12161efe41f42a7d
Tim Lolkema <30321659+tlolkema@users.noreply.github.com>
2019-10-24 14:51:34 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="robotframework-notifications",
+    version="1.0.2",
+    description="Send notifications to Slack or Mattermost using Robot Framework.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tlolkema/RobotNotifications",
+    author="Tim Lolkema",
+    author_email="tim@detesters.nl",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["RobotNotifications"],
+    include_package_data=True,
+    install_requires=["requests"],
+)
